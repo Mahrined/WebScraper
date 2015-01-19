2e203c716131fc8960aeb7bd8e77c9de82f57dcd
Martijn <martijnbos91@gmail.com>
2015-01-19 22:34:38 +0100

    Callbacks are hard.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/scrape', function(req, res){
 	var json = { woorden : "" }
 	var woorden;
 
-	letters = {
+	var alfabet = {
 		'Letter_A' : "A",
 		'Letter_B' : "B",
 		'Letter_C' : "C",
@@ -39,9 +39,9 @@ app.get('/scrape', function(req, res){
 		'Letter_Y' : "Z"
 	};
 
-	for (letter in letters) {
-		var scrape = function( callback ) {
-			var url = 'http://www.woordenlijst.net/woorden_beginnend_met.php?current=' + letters[letter];
+	var scrape = function(callback) {
+		// function scrape(callback) {
+			var url = 'http://www.woordenlijst.net/woorden_beginnend_met.php?current=' + alfabet[letter];
 		    request(url, function(error, response, body) {
 		      if (error) {
 		        return console.error('upload failed:', error);
@@ -52,25 +52,28 @@ app.get('/scrape', function(req, res){
 		      $(links).each(function(i, link){    
 		        var sop = $(this).text();
 		        words[i] = sop;
+				words.push(', '); 
+			    callback(words);
 		      });
-		    words.push(', '); 
-		    if (callback) callback()
+		    // words.push(', '); 
+		    // callback(words);
 		    });
 		}
+		for (letter in alfabet) {
+			    scrape(function(err, words, letter){
+			    	if (err) return console.log(err);
+					json.woorden = words;
+			    	// for (var i=0; i<3; i++){
+			    	// 	console.log("Done! " + i);
+			    	// }
 
+
+				});
+		}		
 		
-	    scrape(function(words){
-	    	for (var i=0; i<2; i++){
-	    		console.log("Done! " + i);
-	    	}
-		});
-	    json.woorden = words;
-
-
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+	 	fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
 			console.log('File successfully written! - Check your project directory for the output.json file');
 		})
-	}
 
 	res.send('Check your console!')
 })
@@ -78,3 +81,22 @@ app.get('/scrape', function(req, res){
 app.listen('8081')
 console.log('Magic happens on port 8081');
 exports = module.exports = app;
+
+
+// // define our function with the callback argument
+// function some_function(arg1, arg2, callback) {
+//     // this generates a random number between
+//     // arg1 and arg2
+//     var my_number = Math.ceil(Math.random() *
+//         (arg1 - arg2) + arg2);
+//     // then we're done, so we'll call the callback and
+//     // pass our result
+//     callback(my_number);
+// }
+// // call the function
+// some_function(5, 15, function(num) {
+//     // this anonymous function will run when the
+//     // callback is called
+//     console.log("callback called! " + num);
+// });
+
