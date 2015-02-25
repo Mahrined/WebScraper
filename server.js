69c769586b96c109dfa78514ca9ce6e191bc883f
Martijn <martijnbos91@gmail.com>
2015-02-25 22:54:09 +0100

    Dutch --> English + iterating over an array instead of an object

--- server.js
+++ server.js
@@ -12,42 +12,42 @@ app.get('/scrape', function(req, res){
 	var json = [];
 	var result = [];
 	var words = [];
-	var alfabet = {
-		'Letter_A' : "A",
-		'Letter_B' : "B",
-		'Letter_C' : "C",
-		'Letter_D' : "D",
-		'Letter_E' : "E",
-		'Letter_F' : "F",
-		'Letter_G' : "G",
-		'Letter_H' : "H",
-		'Letter_I' : "I",
-		'Letter_J' : "J",
-		'Letter_K' : "K",
-		'Letter_L' : "L",
-		'Letter_M' : "M",
-		'Letter_N' : "N",
-		'Letter_O' : "O",
-		'Letter_P' : "P",
-		'Letter_Q' : "Q",
-		'Letter_R' : "R",
-		'Letter_S' : "S",
-		'Letter_T' : "T",
-		'Letter_U' : "U",
-		'Letter_V' : "V",
-		'Letter_W' : "W",
-		'Letter_X' : "X",
-		'Letter_Y' : "Y",
-		'Letter_Z' : "Z"
-	};
+	var dictionary = [
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+		"F",
+		"G",
+		"H",
+		"I",
+		"J",
+		"K",
+		"L",
+		"M",
+		"N",
+		"O",
+		"P",
+		"Q",
+		"R",
+		"S",
+		"T",
+		"U",
+		"V",
+		"W",
+		"X",
+		"Y",
+		"Z"
+	];
 
-	for (var letter in alfabet){
-		var url = 'http://www.woordenlijst.net/woorden_beginnend_met.php?current=' + alfabet[letter];
+	dictionary.forEach(function(letter){
+		var url = 'http://www.woordenlijst.net/woorden_beginnend_met.php?current=' + letter;
 		request(url, function(letter) {
 			return function(err, resp, body) {
 				if (err) throw err;
 
-				console.log("Begonnen met het scrapen van letter: " + alfabet[letter]);
+				console.log("Started scraping letter: " + letter);
 
 				$ = cheerio.load(body);
 				links = $(".productlistingcontentsl a");
@@ -55,9 +55,9 @@ app.get('/scrape', function(req, res){
 		        	words[i] = $(this).text();
 		        });
 		        result.push(words);
-		        console.log("Klaar met het scrapen van letter: " + alfabet[letter]);
+		        console.log("Finished scraping letter: " + letter);
 
-		        if (alfabet[letter] == "Z") {
+		        if (letter == "Z") {
 		       	 	fs.writeFile('output.json', JSON.stringify(result, null, 4), function(err){
 		       	 		if (err) throw err;
 						console.log('File successfully written! - Check your project directory for the output.json file');
@@ -69,10 +69,10 @@ app.get('/scrape', function(req, res){
 	    	request.end();
 
 		}(letter));
-	}
+	});
 
 });
 
 app.listen('8081');
-console.log('Klaar om te scrapen op poort 8081');
+console.log('Ready for scraping on port 8081');
 exports = module.exports = app;
